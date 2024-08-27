768996036f0a4cf89ef5aeaae8c1fa50bc7ed5c2
gwakyerim <jh19930@naver.com>
2024-08-27 21:49:23 +0900

    first commit

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+$(function(){
+  /* Include html Files */
+  $('.header-include').load('../include/header.html')
+  $('.gnb-include').load('../include/gnb.html')
+  
+  /* Slick Slider */
+  $('.main-banner-slider').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+  $('.amuse-pick-slider-inner').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          autoplay: true,
+          autoplaySpeed: 4000,
+        }
+      }
+    ]
+  });
+  $('.best-review-slider-inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+  $('.suggestion-items-inner').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+          }
+        }
+      ]
+  })
+})
