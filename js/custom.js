59752f635aeffa17a5e15779d1114820068966b0
gawkyerim <jh19930@naver.com>
2024-11-19 16:05:07 +0900

    amuse-final

--- js/custom.js
+++ js/custom.js
@@ -2,47 +2,44 @@ $(function(){
   /* Include html Files */
   $('.header-include').load('../include/header.html');
   $('.gnb-include').load('../include/gnb.html');
-
   /* Slick Slider */
-  $('.main-banner-slider').slick({
+  $('.main-wrapper').slick({
     slidesToShow: 1,
     autoplay: true,
     autoplaySpeed: 4000,
+		prevArrow: $('.prev'),
+		nextArrow: $('.next'),
   });
-  $('.amuse-pick-slider-inner').slick({
+  $('.suggest-wrapper').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 4000,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          autoplay: true,
-          autoplaySpeed: 4000,
-        }
-      }
-    ]
+		prevArrow: $('.prev'),
+		nextArrow: $('.next'),
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					autoplaySpeed: 5000,
+				}
+			},
+			{
+				breakpoint:	1023,
+				settings: {
+					slidesToShow: 2,
+					autoplaySpeed: 5000,
+				}
+			}
+		]
   });
-  $('.best-review-slider-inner').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
+	$('.ranking-slider-wrapper').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+		dots: false,
+		arrows: false,
   });
-  $('.suggestion-items-inner').slick({
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 3000,
-          }
-        }
-      ]
-  })
 })
